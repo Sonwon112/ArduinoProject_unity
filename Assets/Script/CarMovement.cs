8d4642a28388842dc33330cce5af62ecc7085f51
Sonwon112 <tlatmsdkqk@gmail.com>
2023-05-19 16:50:09 +0900

    carState 객체 추가  아두이노가 보내는 데이터에따라 상태 변화 작성중

--- Assets/Script/CarMovement.cs
+++ Assets/Script/CarMovement.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class CarMovement : MonoBehaviour
 {
-    
+    // COM9로 연결되는 아두이노 보드 포스 설정    
     SerialPort mPort = new SerialPort("COM9",9600);
     string mdata = null;
-    private string currGear;
-    private int handleRotation;
+    CarState carState = new CarState();
+
     public float speed = 1f;
     public float rotateAngle = 10f;
     float maxAngle = 45f;
 
     public GameObject[] wheel;
     public GameObject[] rotatePoint;
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -25,21 +27,33 @@ public class CarMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float vertical = Input.GetAxis("Vertical");
-        float horizontal = Input.GetAxis("Horizontal");
+        if(carState.getIsStartUp){
+            float vertical = Input.GetAxis("Vertical");
+            float horizontal = Input.GetAxis("Horizontal");
 
-        // Debug.Log(rotateAngle);
-        this.transform.Translate(Vector3.forward*vertical*speed*Time.smoothDeltaTime);
-        if(horizontal < 0){
-            this.transform.Rotate(rotatePoint[0].transform.up,rotateAngle*vertical*horizontal*rotateAngle*Time.smoothDeltaTime);
-        }else if(horizontal > 0){
-            this.transform.Rotate(rotatePoint[1].transform.up,rotateAngle*vertical*horizontal*rotateAngle*Time.smoothDeltaTime);
+            // Debug.Log(rotateAngle);
+            this.transform.Translate(Vector3.forward*vertical*speed*Time.smoothDeltaTime);
+            if(horizontal < 0){
+                this.transform.Rotate(rotatePoint[0].transform.up,rotateAngle*vertical*horizontal*rotateAngle*Time.smoothDeltaTime);
+            }else if(horizontal > 0){
+                this.transform.Rotate(rotatePoint[1].transform.up,rotateAngle*vertical*horizontal*rotateAngle*Time.smoothDeltaTime);
+            }
         }
+        
+
+
+
         
         try{
             if(mPort.IsOpen){
                 mdata = mPort.ReadLine();
-                Debug.Log(mdata);
+                // Debug.Log(mdata);
+                string[] splitData = mdata.split(",");
+                carState.setGear(splitData[0]);
+                carState.setHandleRotation(int.Parse(splitData[1]));
+                carState.setIsAcceleration(int.Parse(splitData[2]));
+                carState.setIsBreak(int.Parse(splitData[3]));
+                carState.setIsStartUp(int.Parse(splitData[4]));
                 mPort.ReadTimeout = 30;
             }
         }catch(Exception e){
@@ -47,4 +61,45 @@ public class CarMovement : MonoBehaviour
         }
 
     }
+}
+
+class CarState{
+    private string currGear = "C";
+    private int handleRotation = "0";
+    private boolean isAcceleration = false;
+    private boolean isBreak = false;
+    private boolean isStartUp = false;
+
+    public void setGear(string gear){
+        currGear = gear;
+    }
+    public void setHandleRotation(int handleRotation){
+        this.handleRotation = handleRotation;
+    }
+    public void setIsAcceleration(boolean isAcceleration){
+        this.isAcceleration = isAcceleration;
+    }
+    public void setIsBreak(boolean isBreak){
+        this.isBreak = isBreak;
+    }
+    public void setIsStartUp(boolean isStartUp){
+        this.isStartUp = isStartUp;
+    }
+
+    public string getGear(){
+        retrun currGear;
+    }
+    public int getHandleRotation(){
+        retrun handleRotation;
+    }
+    public boolean getIsAcceleration(){
+        retrun isAcceleration;
+    }
+    public boolean getIsBreak(){
+        retrun isBreak;
+    }
+    public boolean getIsStartUp(){
+        retrun isStartUp;
+    }
+
 }
